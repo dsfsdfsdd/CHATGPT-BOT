3c16a5e3604e6a4e8f940688b796b8c0c8f8ae2f
Abdubosit <105963406+dsfsdfsdd@users.noreply.github.com>
2023-05-26 21:22:11 +0500

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -2,13 +2,13 @@ import openai
 import os
 
 openai.api_key = os.getenv("OPENAI_API")
-model_engine = "gpt-3.5-turbo"
+model_engine = "text-davinci-003"
 max_tokens = 256
 
 def chatgpt_result(prompt):
     completion = openai.ChatCompletion.create(
         engine=model_engine,
-        prompt=[{"role": "user", "content": prompt}],
+        prompt=prompt
         max_tokens=1024,
         temperature=0.7,
         top_p=1,
